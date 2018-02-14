69b58def48f850d053fc3736ab4ef0f8f639eda4
alkoral <35643632+alkoral@users.noreply.github.com>
2018-02-15 02:04:24 +0300

    Add files via upload

--- admin.php
+++ admin.php
@@ -15,16 +15,26 @@ echo "</pre>";*/
   echo "<h1>Загрузка нового теста</h1>";
 
 if (isset($_FILES['myfile']['name']) && !empty($_FILES['myfile']['name'])) {
-  if ($_FILES['myfile']['error'] == UPLOAD_ERR_OK &&
-    move_uploaded_file($_FILES['myfile']['tmp_name'], "tests/".$_FILES['myfile']['name'])) {
-      echo "Файл <b>".$_FILES['myfile']['name']. "</b> с тестом загружен<br>";
-      $new_file = "<br>Вы можете загрузить новый файл .JSON";
-    }
-    else
-    {
-      echo "Ошибка: файл с тестом не загружен!";
-    }
+    $temp_file = $_FILES['myfile']['tmp_name'];
+    $go_test = file_get_contents($temp_file);
+    $test_res = json_decode($go_test, true);
+      if (!is_array($test_res)) {
+        echo "<font color='red'>Ошибка: это не JSON-файл. Тест на сервер не загружен!</font><br><br>";
+      }
+      else {
+        if (!array_key_exists('quest', $test_res) && !array_key_exists('answ', $test_res)  &&  !array_key_exists('right', $test_res)) {
+          echo "<font color='red'>Ошибка: это JSON-файл, но не тест. Файл на сервер не загружен!</font><br><br>";
+        }
+        else {
+          if ($_FILES['myfile']['error'] == UPLOAD_ERR_OK &&
+          move_uploaded_file($_FILES['myfile']['tmp_name'], "tests/".$_FILES['myfile']['name'])) {
+            echo "<font color='blue'>О чудо! Файл <b>".$_FILES['myfile']['name']. "</b> с тестом загружен</font><br>";
+            $new_file = "<br>Вы можете загрузить новый файл .JSON";
+        }
+     }
+  }
 }
+
 ?>
 
   <form method="post" action="" enctype="multipart/form-data">
